156ca4f7cd126123b2f0d674cfdda8fbe2ed75bb
kamedayasuchika <kame.1230yk.chika@gmail.com>
2025-03-13 14:24:50 +0900

    作成作成

--- custom-event.php
+++ custom-event.php
@@ -1,3 +1,9 @@
+<?php
+/*
+Template Name: イベント投稿ページ
+Template Post Type: post
+*/
+?>
 
 <?php get_header(); ?>
 
@@ -31,7 +37,7 @@
             <div class="tab_panels">
               <!-- Description -->
               <div class="tab_panel">
-                <div class="tab_panel_title"><?php echo $catname; ?></div>
+              <div class="tab_panel_title"><?php echo $catname; ?></div>
                 <div class="tab_panel_content">
                   <div class="tab_panel_text">
                     <div class="news_posts_small">
@@ -39,24 +45,41 @@
                         <div class="col-lg-2 col-md-2 col-sx-12">
                           <div class="calendar_news_border">
                             <div class="calendar_news_border_1">
-                              <div class="calendar_month"><?php echo get_post_time('F'); ?></div>
+                              <div class="calendar_month"><?php echo post_custom('month'); ?></div>
                               <div class="calendar_day">
-                                <span><?php echo get_the_date('d'); ?></span><span>日</span>
+                                <span><?php echo post_custom('day'); ?></span><span>日</span>
                               </div>
                             </div>
                           </div>
+                          <div class="calendar_hour"><?php echo post_custom('time'); ?></div>
                         </div>
                         <div class="col-lg-10 col-md-10 col-sx-12">
                           <div class="news_post_small_header">
-                            <img src="<?php echo get_template_directory_uri( );?>/images/tags-solid.png" alt="" /> <?php echo $catname; ?>
+                          <img src="<?php echo get_template_directory_uri( );?>/images/tags-solid.png" alt="" /> <?php echo $catname; ?>
                           </div>
                           <div class="news_detail_title">
                             <?php the_title(); ?>
                           </div>
+                          <div class="news_time">
+                            <div>
+                              <img src="<?php echo get_template_directory_uri();?>/images/clock-regular.png" alt="" />
+                              <span>日付：<?php echo post_custom('date'); ?> </span>
+                            </div>
+                            <div>
+                              <img src="<?php echo get_template_directory_uri(); ?>/images/yen-sign-solid.png" alt="" />
+                              <span>参加費：<?php 
+                                     if(post_custom('fee') == 0) {
+                                       echo '無料';
+                                     } else {
+                                       echo '¥' . number_format(post_custom('fee'));
+                                     }
+                                   ?></span>
+                            </div>
+                          </div>
                           <div class="news_post_meta">
                             <?php the_content(); ?>
                           </div>
-                          
+
                           <hr />
                           <div class="social_share">
                             <img src="<?php echo get_template_directory_uri(); ?>/images/facebook-square-brands.png" alt=""/>
@@ -71,14 +94,12 @@
             </div>
           </div>
         </div>
+          <?php endwhile; ?>
+          <?php endif; ?>
 
-      <?php endwhile; ?>
-    <?php endif; ?>
         <!--  Sidebar -->
         <div class="col-lg-4" style="background-color: #2b7b8e33">
-          <div class="sidebar">
-            <?php get_sidebar(); ?>
-          </div>
+          <?php get_sidebar(); ?>
         </div>
       </div>
     </div>
